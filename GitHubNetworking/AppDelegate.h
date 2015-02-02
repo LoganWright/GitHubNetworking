60c30f5312155fc26246987ba0cee3ca02e1eb7d
Logan Wright <logan.william.wright@gmail.com>
2015-02-02 16:51:10 -0500

    Initial Commit

---
+++ GitHubNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubNetworking
+//
+//  Created by Logan Wright on 2/2/15.
+//  Copyright (c) 2015 LowriDevs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubNetworking
+//
+//  Created by Logan Wright on 2/2/15.
+//  Copyright (c) 2015 LowriDevs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
